3a2f92754dd4c9319907e64c15daebb615dccd87
kingluqman640-stack <kingluqman640@gmail.com>
2025-11-05 20:43:54 +0400

    coding is cool

---
+++ Exercise 01 - Coding is Cool/coding is cool.py
@@ -0,0 +1,8 @@
+# Define three string variables with the words "Coding", "is", and "cool"
+word1 = "Coding"
+word2 = "is"
+word3 = "cool"
+# Concatenate the strings with spaces in between.
+sentence = word1 + " " + word2 + " " + word3
+# Print the resulting sentence
+print(sentence)
